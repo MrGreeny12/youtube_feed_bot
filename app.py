036d29f3e80a02f215dcee4a3b1f678875730d47
Viktor Ivakhnenko <ivakhnenkovd@yandex.ru>
2022-08-14 11:34:11 +0300

    feat(app) Update base app

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run()
--- telebot/credentials.py
+++ telebot/credentials.py
@@ -6,4 +6,4 @@ env.read_env()
 
 bot_token = env("BOT_TOKEN")
 bot_user_name = env("BOT_USERNAME")
-URL = "the heroku app link that we will create later"
+URL = env("WEBHOOK_URL")
